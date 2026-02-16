79d9b2019de1f162e0b64180084be6f4a42ceaba
Zhasosochka <zassolasso@gmail.com>
2026-02-16 14:29:16 +0500

    Lambda folder

---
+++ Practice3/lambda/lambda_with_sorted.py
@@ -0,0 +1,3 @@
+words = ["apple", "kiwi", "watermelon", "orange"]
+sorted_words = sorted(words, key=lambda x: len(x))
+print(sorted_words)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 REPO for pp2 practice
+
+*Added Practice 1 Examples
+*Added Practice 2 Examples
+*Added Practice 3 Examples
